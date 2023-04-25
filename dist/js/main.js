a568795eb96f1e2d4f5448568ea8df97722ce53c
unknown <jenniroes2801@gmail.com>
2023-04-25 11:09:09 -0600

    html files

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,3 +1,28 @@
 const app = Vue.createApp({
-    
-})
+    data() { 
+        return {
+            recipes: [ 
+                { id: 1, image: "./imgs/unsplash_OFismyezPnY.png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"},
+                { id: 2, image: "./imgs/unsplash_OFismyezPnY(1).png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"},
+                { id: 3, image: "./imgs/unsplash_OFismyezPnY(2).png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"},
+                { id: 4, image: "./imgs/unsplash_OFismyezPnY(3).png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"},
+                { id: 5, image: "./imgs/unsplash_OFismyezPnY(4).png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"},
+                { id: 6, image: "./imgs/unsplash_OFismyezPnY(5).png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"},
+                { id: 7, image: "./imgs/unsplash_OFismyezPnY(6).png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"},
+                { id: 8, image: "./imgs/unsplash_OFismyezPnY(7).png", title: "Recipe title", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago", level: "Easy"}
+            ],
+            recipestop:[
+                {id: 101, image:"./imgs/unsplash_OFismyezPnY(12).png", title:"Recipe title", category: "Entries", level: "Easy", ocassion: "All", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", totaltime: "45min", portions: "4-5", time: "Update 3min ago", position: "#1"},
+                {id: 102, image:"./imgs/unsplash_OFismyezPnY(12).png", title:"Recipe title", category: "Entries", level: "Easy", ocassion: "All", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", totaltime: "45min", portions: "4-5", time: "Update 3min ago", position: "#2"},
+                {id: 103, image:"./imgs/unsplash_OFismyezPnY(12).png", title:"Recipe title", category: "Entries", level: "Easy", ocassion: "All", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", totaltime: "45min", portions: "4-5", time: "Update 3min ago", position: "#3"},
+                {id: 104, image:"./imgs/unsplash_OFismyezPnY(12).png", title:"Recipe title", category: "Entries", level: "Easy", ocassion: "All", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", totaltime: "45min", portions: "4-5", time: "Update 3min ago", position: "#4"}
+            ],
+            relatedrecipes:[
+                {id: 201, image: "./imgs/unsplash_OFismyezPnY(1).png", title: "Recipe title", level: "Easy", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago"},
+                {id: 202, image: "./imgs/unsplash_OFismyezPnY(1).png", title: "Recipe title", level: "Easy", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago"},
+                {id: 203, image: "./imgs/unsplash_OFismyezPnY(1).png", title: "Recipe title", level: "Easy", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "Update 3min ago"},
+                {id: 204, image: "./imgs/unsplash_OFismyezPnY(1).png", title: "Recipe title", level: "Easy", description:"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Loremipsum dolor sit amet, co. Lorem ipsum dolor sit amet, co", time: "sUpdate 3min ago"}
+            ]
+        }
+    }
+});
