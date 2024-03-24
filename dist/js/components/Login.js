4353c059fa207375de15687855da550e3d9f6c0d
Jennifer Rodriguez <jenniroes2801@gmail.com>
2024-03-23 20:32:24 -0600

    Validations in forms Login and Signup

--- dist/js/components/Login.js
+++ dist/js/components/Login.js
@@ -1,50 +1,111 @@
 app.component('login', {
-    methods: {
-      loginUser() {
-        const user = document.getElementById('user').value;
-        const password = document.getElementById('password').value;
-  
+  data() {
+    return {
+      userData: {
+        user: '',
+        password: ''
+      },
+      formSubmitted: false,
+      showIncorrectPasswordAlert: false
+    };
+  },
+  methods: {
+    submitForm() {
+      this.formSubmitted = true;
+    
+      const user = this.userData.user;
+      const password = this.userData.password;
+    
+      if (user && password) {
         axios.post('http://localhost/primerprueba/public/api/users/login', {
           email: user,
           password: password
         })
         .then(response => {
-          localStorage.setItem('token', response.data.accessToken);
-          localStorage.setItem('name', response.data.user.name);
-          localStorage.setItem('lastname', response.data.user.last_name);
-          localStorage.setItem('country', response.data.user.country);
-          localStorage.setItem('email', response.data.user.email);
-          window.location.href = 'http://localhost/TheKitchen/dist/home.html';
+          const userData = response.data.user;
+    
+          if (userData) {
+            localStorage.setItem('token', response.data.accessToken);
+            localStorage.setItem('name', userData.name);
+            localStorage.setItem('lastname', userData.last_name);
+            localStorage.setItem('country', userData.country);
+            localStorage.setItem('email', userData.email);
+    
+            window.location.href = 'http://localhost/TheKitchen/dist/home.html';
+          } else {
+            this.showIncorrectPasswordAlert = true;
+          }
         })
         .catch(error => {
-          console.log(error);
-          window.location.href = 'http://localhost/TheKitchen/dist/error.html';
+          if (error.response && error.response.status === 401) {
+            this.showIncorrectPasswordAlert = true;
+          } else {
+            console.log(error);
+            window.location.href = 'http://localhost/TheKitchen/dist/error.html';
+          }
         });
+      } else {
+        const form = document.getElementById('loginForm');
+        if (form) {
+          form.classList.add('was-validated');
+        }
+
       }
-    },
-    template:
-      /*html*/
-      ` <!--form-->
-      <div class="row container center mt-1 mb-5 col-md-12 margin-box box-width">
-          <!--initial form-->
-          <div class="row container-login col-md-12 center mb-5">
-              <form class="mt-2 mb-2">
-                  <label class="form-label text-forms" for="user">Email</label>
-                  <input id="user" class="form-control" type="text" name="user">
-                  <label for="password" class="form-label mt-4 text-forms">Password <span
-                          class="text-question"><a href="./pass-recovery-one.html" class="text-question"> Forgot?</a></span></label>
-                  <input id="password" type="password" class="form-control color-input">
-                  <div class="col center-button">
-                      <div class="row center">
-                        <a href="#" class="center btn-xl-dark mt-4 hover-grow" @click="loginUser">Log in</a>
-                      </div>
-                      <div>
-                          <span class=" line-deco mt-4 mb-3 mx-auto"></span>
-                      </div>
-                      <div class="row center"><a href="./signup.html" class="center btn-xl-light mt-2 hover-grow">Sign up</a></div>
-                  </div>
-              </form>
+    }    
+
+  },
+  template:
+    /*html*/
+    `
+    <!--Alert incorrect answer-->
+    <div v-if="showIncorrectPasswordAlert" class="alert-incorrect mb-5" role="alert">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill me-4" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5m.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
+      </svg>
+      <div>Incorrect password or user. Please try again!</div>
+    </div>
+    <!--form-->
+    <div id="loginForm" class="row container center mt-1 mb-5 col-md-12 margin-box box-width">
+      <div class="row container-login col-md-12 center mb-5">
+        <form class="mt-2 mb-2 needs-validation" @submit.prevent="submitForm" novalidate>
+          <label class="form-label text-forms" for="user">Email</label>
+          <input id="user" class="form-control" type="text" name="user" v-model="userData.user" required>
+          <div class="invalid-feedback">Please enter your email</div>
+
+          <label for="password" class="form-label mt-4 text-forms">Password <span class="text-question"><a href="./pass-recovery-one.html" class="text-question"> Forgot?</a></span></label>
+          <input id="password" type="password" class="form-control color-input" v-model="userData.password" required >
+          <div class="invalid-feedback is-invalid">Please enter your password</div>
+
+          <div class="col center-button">
+            <div class="row center">
+              <button type="submit" class="center btn-xl-dark mt-4 hover-grow">Log in</button>
+            </div>
+            <div>
+              <span class="line-deco mt-4 mb-3 mx-auto"></span>
+            </div>
+            <div class="row center">
+              <a href="./signup.html" class="center btn-xl-light mt-2 hover-grow">Sign up</a>
+            </div>
           </div>
-          <!--final form-->
-      </div>`
-  });
+        </form>
+      </div>
+    </div>
+  `
+});
+
+(() => {
+  'use strict'
+
+  const forms = document.querySelectorAll('.needs-validation')
+
+  Array.from(forms).forEach(form => {
+    form.addEventListener('submit', event => {
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
+      }
+
+      form.classList.add('was-validated')
+    }, false)
+  })
+})()
--- dist/js/components/Signup.js
+++ dist/js/components/Signup.js
@@ -8,6 +8,7 @@ app.component('signup', {
             const email = document.getElementById('email').value;
             const password = document.getElementById('password').value;
 
+            if (name && lastName && country && email && password) {
             //Create the data object to send in the POST request
             const data = {
                 name: name,
@@ -17,47 +18,75 @@ app.component('signup', {
                 password: password
             };
 
-            axios.post('http://localhost/primerprueba/public/api/users/register', data)
-                .then(response => {    
-                    window.location.href = 'http://localhost/TheKitchen/dist/confirm.html';
-                })
-                .catch(error => {
-                    console.error(error);
+                axios.post('http://localhost/primerprueba/public/api/users/register', data)
+                    .then(response => {
+                        window.location.href = 'http://localhost/TheKitchen/dist/confirm.html';
+                    })
+                    .catch(error => {
+                        console.error(error);
 
-                    console.log(response);
-                });
+                        console.log(response);
+                    });
+            } else {
+                const form = document.getElementById('signupForm');
+                if (form) {
+                    form.classList.add('was-validated');
+                }
+            }
         }
     },
     template:
-      /*html*/
-      `<!--form-->
+        /*html*/
+        `
     <div class="container container-signup mb-5">
-    <!--name-->
-    <div class="m-3 p-2">
+    <!--form-->
+    <form id="signupForm" class="m-3 p-2 needs-validation" novalidate>
         <label class="form-label mt-3" for="name">Name</label>
-        <input id="name" class="form-control mb-2" type="text" name="name">
+        <input id="name" class="form-control mb-2" type="text" name="name" required>
+        <div class="invalid-feedback">Please enter your name</div>
         <!--lastname-->
         <label class="form-label" for="lastname">Lastname</label>
-        <input id="lastname" class="form-control mb-2" type="text" name="lastname">
+        <input id="lastname" class="form-control mb-2" type="text" name="lastname" required>
+        <div class="invalid-feedback">Please enter your lastname</div>
         <!--country-->
         <label class="form-label" for="country">Country</label>
-        <input id="country" class="form-control mb-2" type="text" name="country">
+        <input id="country" class="form-control mb-2" type="text" name="country" required>
+        <div class="invalid-feedback">Please enter your country</div>
         <!--email-->
         <label class="form-label" for="email">Email</label>
-        <input id="email" class="form-control mb-2" type="email" name="email">
+        <input id="email" class="form-control mb-2" type="email" name="email" required>
+        <div class="invalid-feedback">Please enter your email</div>
         <!--password-->
         <label for="password" class="form-label">Password</label>
-            <input id="password" class="form-control mb-2" type="password">
-            
+        <input id="password" class="form-control mb-2" type="password" required>
+        <div class="invalid-feedback">Please enter your password</div>
+                
         <!--buttons-->
         <div class="col center-button">
-            <div class="">
             <div class="row center"><a class="center btn-signup-dark mt-4 mb-3 hover-grow" @click="registerUser">Sign up</a></div>
-                <span class=" line-deco mb-3 mx-auto"></span>
-                <div class="row center"><a href="login.html" class="center btn-signup-light mb-4 hover-grow">Log in</a></div>
+            <div>
+                <span class="line-deco mb-3 mx-auto"></span>
             </div>
-            
-        </div>
-    </div>
-</div>`
+            <div class="row center"><a href="login.html" class="center btn-signup-light mb-4 hover-grow">Log in</a></div>
+        </div>              
+    </form>        
+</div>
+`
 });
+
+(() => {
+    'use strict'
+
+    const forms = document.querySelectorAll('.needs-validation')
+
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
